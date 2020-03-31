36222b928ad80d0d77f9d97d302ead15e49810ce
Martin Palmer <martin.palmer@almmechanics.com>
2020-03-31 13:39:04 +0100

    update pre-asigned codebase

--- app_service/python/PreAssignedSaS/__init__.py
+++ app_service/python/PreAssignedSaS/__init__.py
@@ -6,21 +6,23 @@ import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Pre-assigned SAS')
-    logging.info(os.environ['SHARED_ACCESS_SIGNATURE'])        
+    logging.info(os.environ['SHARED_ACCESS_SIGNATURE'])       
+    
+    return func.HttpResponse(os.environ['SHARED_ACCESS_SIGNATURE'])
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    # name = req.params.get('name')
+    # if not name:
+    #     try:
+    #         req_body = req.get_json()
+    #     except ValueError:
+    #         pass
+    #     else:
+    #         name = req_body.get('name')
 
-    if name:
-        return func.HttpResponse(f"Hello {name}!")
-    else:
-        return func.HttpResponse(
-             "Please pass a name on the query string or in the request body",
-             status_code=400
-        )
+    # if name:
+    #     return func.HttpResponse(f"Hello {name}!")
+    # else:
+    #     return func.HttpResponse(
+    #          "Please pass a name on the query string or in the request body",
+    #          status_code=400
+    #     )
